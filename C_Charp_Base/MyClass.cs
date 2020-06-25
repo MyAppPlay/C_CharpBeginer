cd106071fe3abb07143637e602d36ab342d5d831
MyAppPlay <50810271+MyAppPlay@users.noreply.github.com>
2020-06-25 04:44:33 +0300

    Добавьте файлы проекта.

---
+++ C_Charp_Base/MyClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+
+namespace C_Charp_Base
+{
+    class MyClass
+    {
+        public static void Pause()
+        {
+            Console.WriteLine("Для продолжения нажмите любую кнопку");
+            Console.ReadKey();
+        }
+
+        public static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void Resume(string Name1, string Name2, string Town)
+        {
+            var l = (Name1.Length + Name2.Length + Town.Length) / 2;
+            Console.SetWindowSize(Console.LargestWindowWidth/2, Console.LargestWindowHeight/2);
+            Console.SetCursorPosition(Console.WindowWidth/2 - l, 1);
+            Console.WriteLine($"{Name1} {Name2}, {Town}");
+        }
+
+        public static void SwitchValue()
+        {
+            Console.WriteLine("Напишите первое значение");
+            var a = Console.ReadLine();
+            Console.WriteLine("Напишите второе значение");
+            var b = Console.ReadLine();
+            var c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"{a}, {b}");
+        }
+
+        public static void SwitchValueEasy()
+        {
+            Console.WriteLine("Внимание! Этот метод подходит только для целых чисел");
+            Console.WriteLine("Напишите первое значение");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Напишите второе значение");
+            var b = int.Parse(Console.ReadLine());
+            a += b;
+            b = a - b;
+            a -= b;
+            Console.WriteLine($"{a}, {b}");
+        }
+
+        public static void VectorLength()
+        {
+            Console.WriteLine("Введите координату x1");
+            var x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату x2");
+            var x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y1");
+            var y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y2");
+            var y2 = double.Parse(Console.ReadLine());
+            var r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Длина вектора равняется  {r:F2}");
+        }
+
+        public static void IMT()
+        {
+            Console.WriteLine("Какой у тебя рост?");
+            var h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Какой у тебя вес?");
+            var m = double.Parse(Console.ReadLine());
+            var I = m / (h * h);
+            Console.WriteLine($"ИМТ составляет {I}");
+        }
+
+        public static int  Random(int x, int y)
+        {
+            var r = new Random();
+            return r.Next(x, y);
+        }
+
+        public static void ConsoleBackgroundAndForegroundColor()
+        {
+                Console.BackgroundColor = (ConsoleColor)Random(13,16);
+                Console.ForegroundColor = (ConsoleColor)Random(0, 6);
+        }
+    }
+}
---
+++ C_Charp_Base/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static C_Charp_Base.MyClass;
+
+namespace C_Charp_Base
+{
+    class Program
+    {
+        #region Constants
+
+        const string V1 = "\t1.Программа обмена значениями переменных";
+        const string V1A = "\ta.С использованием третей переменной";
+        const string V1B = "\tb.Без использования третьей переменной\n";
+        const string V2 = "\t2.Определение длины отрезка по координатам\n";
+        const string V3 = "\t3.Определение ИМТ\n";
+
+        #endregion
+
+
+        static void Main()
+        {
+            string value;
+            do
+            {
+
+                ConsoleBackgroundAndForegroundColor();
+                Console.Clear();
+
+                Resume("Владимир", "Гаврилов", "Москва");
+
+                Console.WriteLine($"{V1}\n\t{V1A}\n\t{V1B}\n{V2}\n{V3}");
+                Console.WriteLine("Для продолжения наберите номер пункта.(Например \"1a\")\nДля выхода из программы наберите цифру 0");
+                value = Console.ReadLine();
+
+                if (value == "1a")
+                    SwitchValue();
+                else if (value == "1b")
+                    SwitchValueEasy();
+                else if (value == "2")
+                    VectorLength();
+                else if (value == "3")
+                    IMT();
+                else Console.WriteLine("Вы набрали не существующий пункт меню");
+                Pause();
+            }
+            while (value != "0");
+        }
+    }
+}
